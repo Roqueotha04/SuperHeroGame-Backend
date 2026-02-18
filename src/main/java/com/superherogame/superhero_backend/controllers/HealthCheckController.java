1c37418a617af6df7d949cab448783397dd51346
roqueotha04 <roqueotha04@gmail.com>
2026-02-18 13:44:17 -0300

    feat: add HealthController and endpoint active/status

---
+++ src/main/java/com/superherogame/superhero_backend/controllers/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.superherogame.superhero_backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/api/status")
+    public String checkStatus() {
+        return "ok";
+    }
+}
